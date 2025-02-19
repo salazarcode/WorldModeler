df159cdc22e02e11276a7ebb972dd1de8ff9eece
Andrés Salazar <salazarcode@gmail.com>
2025-02-19 13:24:41 -0400

    Add project files.

---
+++ Infrastructure.Repository.MSSQL.EF/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repository.MSSQL.EF
+{
+    public class Context : DbContext
+    {
+        private readonly string _connectionString;
+
+        public Context(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_connectionString);
+            }
+        }
+    }
+}
